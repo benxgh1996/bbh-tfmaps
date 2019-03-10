81fabacb7763400561456651ad5949edf6e95e18
gxu36 <benxgh1996@gmail.com>
2019-03-10 16:25:07 -0400

    added svm classifier

---
+++ Classifier.py
@@ -0,0 +1,243 @@
+import numpy as np
+from sklearn import svm, metrics
+from skimage import feature, filters
+from sklearn.model_selection import cross_validate
+import time
+import matplotlib.pyplot as plt
+from DataFactory import DataFactory
+import matplotlib.pyplot as plt
+
+
+class Classifier:
+    def __init__(self, classifier=svm.SVC(kernel="linear", gamma="auto"),
+                 featSet=None, labelSet=None, imSet=None):
+        self.classifier = classifier
+        self.featSet = featSet
+        self.labelSet = labelSet
+        self.imSet = imSet
+
+    # Loads the labelled data as input images and input labels.
+    # @param fileName (str)
+    def loadData(self, fileName):
+        data = np.load(fileName)
+        self.imSet, self.labelSet = DataFactory.getTrainableArrays(data)
+        self.labelSet = np.array(self.labelSet)
+        assert isinstance(self.imSet[0, 0, 0], np.floating)
+        assert (self.imSet >= 0).all() and (self.imSet <= 1).all()
+
+    # This function extracts the features from the image set. By default
+    #   we will not apply the Gaussian filter.
+    # @param sigma (float): If None, do not apply the Gaussian filter
+    #   to smooth the images. May want to try sigma = 0.6.
+    def extractFeats(self, flatten=False, sigma=None):
+        # print "Number of instances:", len(self.imSet)
+        if sigma is not None:
+            # Smooth the image set.
+            smoothSet = []
+            for im in self.imSet:
+                smoothIm = filters.gaussian(im, sigma=sigma)
+                smoothSet.append(smoothIm)
+            # Rewrites self.imSet.
+            self.imSet = np.array(smoothSet)
+            assert self.imSet.ndim == 3
+            assert isinstance(self.imSet[0, 0, 0], np.floating)
+            assert (self.imSet >= 0).all() and (self.imSet <= 1).all()
+
+        # print "Extracting features.."
+        if flatten:
+            self.featSet = self.imSet.reshape(len(self.imSet), -1)
+            assert len(self.featSet) == len(self.imSet)
+            assert np.array_equal(self.featSet[0], self.imSet[0].flatten())
+        else:
+            # A collection that holds the feature vectors.
+            self.featSet = []
+            # orient, cells_p, pixels_p, vec_len
+            # 8, (4, 4), (4, 4), 21632
+            # 8, (4, 4), (8, 8), 3200
+            # 8, (2, 2), (8, 8),
+            # 8, (2, 2), (4, 4), 7200
+            for im in self.imSet:
+                feat = feature.hog(im, orientations=8, pixels_per_cell=(8, 8),
+                                   cells_per_block=(2, 2), block_norm="L2-Hys")
+                self.featSet.append(feat)
+            self.featSet = np.array(self.featSet)
+        assert self.featSet.ndim == 2
+
+    # Checks the properties of the extracted image feature.
+    def checkFeat(self):
+        im = self.imSet[0]
+        print "Shape of image:", im.shape
+        for im in self.imSet:
+            feat = feature.hog(im, orientations=8, pixels_per_cell=(8, 8),
+                               cells_per_block=(2, 2), block_norm="L2-Hys")
+
+        print("Feature shape:", feat.shape)
+        print("Feature type:", feat.dtype)
+        print("Max feat value:", feat.max())
+        print("min feat value:", feat.min())
+        # plt.imshow(im, cmap="gray")
+        # plt.show()
+        # plt.imshow(hogIm, cmap="gray")
+        # plt.show()
+
+    # Resets the classifier.
+    def setClassifier(self, classifier):
+        self.classifier = classifier
+
+    # Trains the classifier.
+    def train(self):
+        assert self.featSet.ndim == 2
+        assert self.labelSet.ndim == 1
+        assert len(self.featSet) == len(self.labelSet)
+        print("Training classifier..")
+        print("Length of feature set:", len(self.featSet))
+        self.classifier.fit(self.featSet, self.labelSet)
+
+    # Predicts the labels of a collection of images.
+    # @param data (array<*, *>): A list of features representing images
+    #   to be classified.
+    # @return array: An array of predicted labels.
+    def predictLabels(self, data):
+        return self.classifier.predict(data)
+
+    # This functions performs k-fold cross-validation on the training data.
+    # @param trainArr (array<array>): The training features.
+    # @param labelArr (array<str-like>): The training labels.
+    # @param k (int): The number of folds for k-fold cross-validation.
+    def crossVal(self, k=5):
+        assert self.featSet.ndim == 2
+        assert self.labelSet.ndim == 1
+        assert len(self.featSet) == len(self.labelSet)
+        assert isinstance(self.labelSet[0], str)
+        scores = cross_validate(self.classifier, self.featSet,
+                                self.labelSet, cv=k, return_train_score=True)
+        trainAccus = scores["train_score"]
+        testAccus = scores["test_score"]
+        assert trainAccus.shape == (k, )
+        print "On training set"
+        print "Accuracies:", trainAccus
+        print "Accuracy means:", trainAccus.mean()
+        print "Accuracy Std:", trainAccus.std()
+        print
+        print "On validation set"
+        print "Accuracies:", testAccus
+        print "Accuracy means:", testAccus.mean()
+        print "Accuracy Std:", testAccus.std()
+
+    # Testing the effect of applying an image filter.
+    def testFilter(self, sigma=1):
+        im = self.imSet[0]
+        assert im.ndim == 2
+        assert isinstance(im[0, 0], np.floating)
+        assert im.max() <= 1
+        assert im.min() >= 0
+
+        newIm = filters.gaussian(im, sigma=sigma)
+        assert isinstance(newIm[0, 0], np.floating)
+        assert newIm.max() <= 1
+        assert newIm.min() >= 0
+        plt.imshow(np.flip(im, axis=0), cmap="gray")
+        plt.title("Original Image")
+        plt.show()
+        plt.imshow(np.flip(newIm, axis=0), cmap="gray")
+        plt.title("New Image, sigma = {}".format(sigma))
+        plt.show()
+
+
+# def main():
+#     clf = Classifier()
+#
+#     # load images
+#     clf.loadData("newTrainSet.npy")
+#     (test_raw, test_labels) = clf.loadData('./test/')
+#
+#     # convert images into features
+#     # Notice that an entire array of images are passed into the feature
+#     # extraction function.
+#     # train_data, test_data are both arrays of features that represent
+#     # the collection of images.
+#     train_data = clf.extractFeats(train_raw)
+#     test_data = clf.extractFeats(test_raw)
+#
+#     # train model and test on training data
+#     clf.train(train_data, train_labels)
+#
+#     # Now that we have trained the classifier.
+#     # We want to predict the labels of the training data to obtain
+#     # the training accuracy.
+#     predicted_labels = clf.predictLabels(train_data)
+#     print("\nTraining results")
+#     print("=============================")
+#     print("Confusion Matrix:\n",
+#           metrics.confusion_matrix(train_labels, predicted_labels))
+#     print("Accuracy: ",
+#           metrics.accuracy_score(train_labels, predicted_labels))
+#     print("F1 score: ", metrics.f1_score(
+#         train_labels, predicted_labels, average='micro'))
+#
+#     # test model
+#     predicted_labels = clf.predictLabels(test_data)
+#     print("\nTest results")
+#     print("=============================")
+#     print("Confusion Matrix:\n",
+#           metrics.confusion_matrix(test_labels, predicted_labels))
+#     print("Accuracy: ",
+#           metrics.accuracy_score(test_labels, predicted_labels))
+#     print("F1 score: ",
+#           metrics.f1_score(test_labels, predicted_labels, average='micro'))
+
+
+# This function is used for personal testing.
+def crossVal(k=5):
+    clf = Classifier()
+    # Loading training data
+    print "Loading training data.."
+    startLoadTime = time.time()
+    clf.loadData("heavyTrainSet.npy")
+    endLoadTime = time.time()
+    loadTime = endLoadTime - startLoadTime
+    print "Training data load time:", loadTime, "sec"
+    print
+
+    # # Checking feature vector.
+    # print "Checking features.."
+    # clf.checkFeat()
+    # print
+
+    # Extracting features.
+    print "Extracting features from training data.."
+    startExtractTime = time.time()
+    # clf.extractFeats()
+    clf.extractFeats(flatten=True)
+    endExtractTime = time.time()
+    extractTime = endExtractTime - startExtractTime
+    print "Training data feature extraction time:", extractTime, "sec"
+    print
+
+    # Cross-validation.
+    print "Doing {} fold cross-validation..".format(k)
+    print("-" * 50)
+    startCrossTime = time.time()
+    clf.crossVal(k=k)
+    endCrossTime = time.time()
+    crossTime = endCrossTime - startCrossTime
+    print("Cross-validation time:", crossTime, "sec")
+
+
+# Used for testing the filtering effect.
+def checkFilter(sigma=1):
+    clf = Classifier()
+    clf.testFilter(sigma=sigma)
+
+
+# Used for testing the HOG feature vector.
+def checkFeat():
+    clf = Classifier()
+    clf.loadData("heavyTrainSet.npy")
+    clf.checkFeat()
+
+
+if __name__ == "__main__":
+    # main()
+    crossVal(k=5)
+    # checkFeat()
